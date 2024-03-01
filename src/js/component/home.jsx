dd4345a2d46c843f933973fb34ef5d995b53f54f
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-03-01 15:19:55 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
-export default Home;
+	const [list, setList] = useState([{ label: '', done: false }])
+
+	const [task, setTask] = useState("")
+
+
+
+	const createUser = async () => {
+
+		const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/Jean-abiad', {
+			method: 'POST',
+			body: JSON.stringify([]),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+		})
+		if (response.ok) {
+			getTasks()
+		}
+	}
+	// createUser()
+
+
+
+	const getTasks = async () => {
+		try {
+			const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/Jean-abiad')
+			const data = await response.json()
+			if (response.ok) {
+				setList(data)
+			} else {
+				//crear user
+				createUser()
+			}
+
+		}
+		catch (error) {
+			console.log(error);
+		}
+	}
+	// getTasks()
+
+	const handleSubmit = async (evt) => {
+
+
+		if (evt.key == "Enter") {
+			const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/Jean-abiad', {
+				method: 'PUT',
+				body: JSON.stringify([...list, { label: task, done: false }]),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+
+			if (response.ok) {
+				// getTasks()
+
+			}
+		}
+	};
+
+
+	useEffect(() => {
+
+		getTasks()
+
+	}, [])
+
+
+	return <>
+		<form onSubmit={(evt) => { evt.preventDefault() }}>
+
+			<input placeholder="Write a Task" className="m-3" type="text"
+				onKeyUp={handleSubmit}
+				onChange={(e) => setTask(e.target.value)}
+			/>
+		</form>
+
+		{list && list.map((task, index) => <div className="d-flex gap-2 m-3" key={index}>
+			<h2>- {task.label} -</h2>
+			<button className="btn btn-danger">Delete</button>
+		</div>
+
+		)}
+
+	</>
+
+
+
+}
+export default Home; 
